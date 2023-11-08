90e3bcefa50d13689253e8eefd5b16d70c5476e1
SanyaMar <eklisof@gmail.com>
2023-11-09 00:38:39 +0300

    Приведено в приличный вид и исправлены недочеты

--- Code.py
+++ Code.py
@@ -9,32 +9,39 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 
-def creating_folder(name):
+def creating_folder(name: str) -> None:
     if not os.path.isdir(name):
-         os.mkdir(name)
+        os.mkdir(name)
 
 
-def getting_links(request):
+def getting_links(request: str) -> None:
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.maximize_window()
+    time.sleep(2)
     url = f"https://yandex.ru/images/search?text={request}"
     driver.get(url=url)
-    driver.maximize_window()
-    time.sleep(10)
-    driver.find_element(By.CSS_SELECTOR, 'div.serp-item__preview a.serp-item__link').click()
-    with open(f"urls_{request}.txt", 'w') as file:
-        for i in range(15):
+    time.sleep(3)
+    driver.find_element(
+        By.CSS_SELECTOR, "div.serp-item__preview a.serp-item__link"
+    ).click()
+    with open(f"urls_{request}.txt", "w") as file:
+        for i in range(5):
             try:
                 time.sleep(5)
-                link = driver.find_element(By.CSS_SELECTOR, "a.Button2_view_action").get_attribute("href")
-                file.write(link + '\n')
-                driver.find_element(By.CSS_SELECTOR, "div.CircleButton:nth-child(4)").click()
+                link = driver.find_element(
+                    By.CSS_SELECTOR, "a.Button2_view_action"
+                ).get_attribute("href")
+                file.write(link + "\n")
+                driver.find_element(
+                    By.CSS_SELECTOR, "div.CircleButton:nth-child(4)"
+                ).click()
             except:
                 continue
     driver.close()
     driver.quit()
 
 
-def download_images(images):
+def download_images(images: str) -> None:
     count = 0
 
     creating_folder("dataset")
@@ -47,26 +54,31 @@ def download_images(images):
                 time.sleep(10)
                 response = requests.get(url, stream=True)
                 if response.status_code == 200:
-                    count+=1
-                    with open(f"dataset/{images}/{str(count).zfill(4)}.jpg", "wb") as image_file:
+                    count += 1
+                    with open(
+                        f"dataset/{images}/{str(count).zfill(4)}.jpg", "wb"
+                    ) as image_file:
                         shutil.copyfileobj(response.raw, image_file)
                 else:
                     continue
             except:
                 continue
-    print(f'{count} Images downloaded successfully')        
+    print(f"{count} Images downloaded successfully")
 
 
-def main():
+def main() -> None:
     if os.path.isdir("dataset"):
         shutil.rmtree("dataset")
-    request = "cat"
-    getting_links(request)
-    download_images(request)
+   # request = "cat"
+   # getting_links(request)
+   # time.sleep(5)
+   # download_images(request)
+   # time.sleep(10)
     request = "dog"
     getting_links(request)
+    time.sleep(5)
     download_images(request)
 
 
 if __name__ == "__main__":
-    main()      
+    main()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from code import main
+from Code import main
 
 if __name__ == "__main__":
     main()
